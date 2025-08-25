a4b66f34e266c80000e431f5dad73d0c6a597d05
ayyanar <ppayyanar53@gmail.com>
2025-08-25 13:07:22 +0530

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
               <p className="text-lg text-foreground leading-relaxed">
                <div className="flex justify-center animate-fade-in-up">
     <img
-      src="https://github.com/ayyanar-03/port/blob/master/src/assets/ayyanar.jpg"
+      src="https://github.io/ayyanar-03/port/src/assets/ayyanar.jpg"
       alt=""
       className="w-48 h-48 rounded-full object-cover shadow-lg"
     />
